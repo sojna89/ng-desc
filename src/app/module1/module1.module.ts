17b647887b02f07ddee766a29252fdde71e66d44
Rodrigo Anjos <rodrigo.anjos@externos-br.cencosud.com>
2024-05-01 20:00:03 -0300

    feat(link): creating link component

--- src/app/module1/module1.module.ts
+++ src/app/module1/module1.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { Module1RoutingModule } from './module1-routing.module';
 import { Module1Component } from './module1.component';
 import { Child1Component } from './child1/child1.component';
+import { LinkComponent } from '../components/link/link.component';
 
 
 @NgModule({
   declarations: [
     Module1Component,
-    Child1Component
+    Child1Component,
+    LinkComponent
   ],
   imports: [
     CommonModule,
